251f0d87c5b770f08b502673a233e659fb006d86
Will Acton <will@amperity.com>
2024-04-16 17:04:41 -0700

    init

---
+++ src/app/a.cljs
@@ -0,0 +1,6 @@
+(ns app.a
+  (:require
+    [app.b :as-alias b]))
+
+
+(def data #::b {:x 1 :y 2 :z 3})
---
+++ src/app/main.cljs
@@ -0,0 +1,6 @@
+(ns app.main
+  (:require
+    [app.a :as a]))
+
+(defn init! []
+  (js/console.log "Hello, world!"))
